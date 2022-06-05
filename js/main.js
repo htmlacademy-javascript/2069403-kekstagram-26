8cba5e993d2711e9975e22f904306534cb6d9239
Yulya <iuliiaobr.io@gmail.com>
2022-06-05 17:10:03 +0300

    Последняя версия

--- js/main.js
+++ js/main.js
@@ -1,41 +1,25 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-const isNumber = (...values) => typeof values === 'number';
-isNumber();
-
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+const isNumber = (value) => typeof value === 'number';
 
 const getRandonNum = (min, max) => {
   // если не числа
   if(!isNumber(min) || !isNumber(max)) {
-    throw new Error('I can\'t compare different types');
-
-  }
-  // если оба меньше 0
-  if(min < 0 && max < 0) {
-    return 'Sorry just positive numbers';
+    return -1;
   }
 
-  min = Math.floor(min);
-  max = Math.floor(max);
+  min = Math.floor(Math.abs(min));
+  max = Math.floor(Math.abs(max));
 
-  // если только min меньше 0
-  if(min < 0 && max >= 0) {
-    min = 0;
+  if(min === max) {
+    return Math.floor(min); // если одинаковые числа
   }
 
-  // если оба больше 0
-  if(min >= 0 && max >= 0) {
-    if(min === max) {
-      return Math.floor(min); // если одинаковые числа
-    }
-
-    if(min > max) {
-      [min, max] = [max, min]; // если max меньше min, то меняю их местами
-    }
-
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-
+  if(min > max) {
+    [min, max] = [max, min]; // если max меньше min, то меняю их местами
   }
 
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 getRandonNum();
