ef35457ba658145cd37fd87e42796950ea187350
Yulya <iuliiaobr.io@gmail.com>
2022-06-06 04:46:37 +0300

    Добавляет варианты функции для проверки максимальной длины строки

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 const isNumber = (value) => typeof value === 'number';
 
 const getRandonNum = (min, max) => {
@@ -23,3 +22,19 @@ const getRandonNum = (min, max) => {
 };
 
 getRandonNum();
+
+// option 1. есть константа length (140)
+const MAX_LENGTH = 140;
+
+const checkCommentLength = (userComment) => userComment.length <= MAX_LENGTH;
+
+checkCommentLength();
+
+
+// имя_функции(проверяемая_строка, максимальная_длина); // Результат: true, если строка проходит по длине, и false — если не проходит
+const checkAnotherComment = (userComment, maxCommentLenght) => {
+  maxCommentLenght = 140;
+  return userComment.length <= maxCommentLenght;
+};
+
+checkAnotherComment();
