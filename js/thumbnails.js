bef7d31d5e2d79bc8c17a8f9edaa86a397f6b666
Yulya <iuliiaobr.io@gmail.com>
2022-06-26 11:24:44 +0700

    Заполнение полноразмерного фото данными

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -3,18 +3,38 @@ const thumbnailTemplateContainer = document.querySelector('#picture').content.qu
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureCancel = bigPicture.querySelector('.big-picture__cancel');
 
-const showBigPicture = (photo) => {
+const commentList = (comments) => {
+  const commentBlock = bigPicture.querySelector('.social__comments');
+  comments.forEach((comment) => {
+    commentBlock.querySelector('img').src = comment.avatar;
+    commentBlock.querySelector('img').alt = comment.name;
+    commentBlock.querySelector('.social__text').textContent = comment.message;
+  });
+};
+
+const showBigPicture = (url, likes, comments, description) => {
   bigPicture.classList.remove('hidden');
-  bigPicture.querySelector('.big-picture__img').src = photo.url;
+  const commentCount = bigPicture.querySelector('.social__comment-count');
+  commentCount.classList.add('hidden');
+  const loadNewPic = bigPicture.querySelector('.comments-loader');
+  loadNewPic.classList.add('hidden');
+  const scrollBackground = document.querySelector('body');
+  scrollBackground.classList.add('modal-open');
+
+  bigPicture.querySelector('img').src = url;
+  bigPicture.querySelector('.likes-count').textContent = likes;
+  bigPicture.querySelector('.comments-count').textContent = comments.length;
+  bigPicture.querySelector('.social__caption').textContent = description;
+  commentList(comments);
 
 };
 
-const renderPhoto = ({url, likes, comments}) => {
+const renderPhoto = ({url, likes, comments, description}) => {
   const photoItem = thumbnailTemplateContainer.cloneNode(true); // клониурем
   photoItem.querySelector('img').src = url;
   photoItem.querySelector('.picture__likes').textContent = likes;
   photoItem.querySelector('.picture__comments').textContent = comments.length;
-  photoItem.addEventListener('click', showBigPicture);
+  photoItem.addEventListener('click', () => showBigPicture(url, likes, comments, description));
   return photoItem;
 };
 
