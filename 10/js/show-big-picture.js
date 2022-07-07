a932452a7d7598e1f12983ffab3bbec00c977e4b
Keks <keks@htmlacademy.ru>
2022-07-07 09:18:02 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/show-big-picture.js
@@ -0,0 +1,83 @@
+import { isEscapeKey } from './utile.js';
+
+const bigPicture = document.querySelector('.big-picture');
+const bigPictureCancel = bigPicture.querySelector('.big-picture__cancel');
+const scrollBackground = document.querySelector('body');
+const commentsList = bigPicture.querySelector('.social__comments');
+const commentTemplate = document.querySelector('#social-comment').content.querySelector('.social__comment');
+const displayedCommentNumber = bigPicture.querySelector('.displayed-comment-number');
+
+const showMoreCommentsButton = bigPicture.querySelector('.comments-loader');
+
+
+const onBigPuctureEscpaeKey = (evt) => {
+  if (isEscapeKey(evt)) {
+    userBigPictureCloseElement(evt);
+  }
+};
+
+function userBigPictureCloseElement(evt) {
+  evt.preventDefault();
+  bigPicture.classList.add('hidden');
+  scrollBackground.classList.remove('modal-open');
+  document.removeEventListener('keydown', onBigPuctureEscpaeKey);
+}
+
+const renderComment = ({avatar, name, message}) => {
+  const commentBlock = commentTemplate.cloneNode(true);
+  commentBlock.querySelector('img').src = avatar;
+  commentBlock.querySelector('img').alt = name;
+  commentBlock.querySelector('.social__text').textContent = message;
+  return commentBlock;
+};
+
+const renderComments = (comments) => commentsList.append(...comments.map(renderComment));
+
+let Comments = [];
+let currentDisplayedComments = 0;
+
+const showMoreComments = () => {
+  if (Comments.length <= 5) {
+    showMoreCommentsButton.classList.add('hidden');
+  }
+  const forRendering = Comments.slice(0, 5);
+  Comments = Comments.slice(5);
+  renderComments(forRendering);
+
+  currentDisplayedComments += forRendering.length;
+  displayedCommentNumber.textContent = currentDisplayedComments;
+};
+
+const renderBigPicture = (url, likes, comments, description) => {
+  bigPicture.querySelector('img').src = url;
+  bigPicture.querySelector('.likes-count').textContent = likes;
+  bigPicture.querySelector('.comments-count').textContent = comments.length;
+  bigPicture.querySelector('.social__caption').textContent = description;
+
+  showMoreCommentsButton.classList.remove('hidden');
+  Comments = comments;
+  showMoreComments(Comments);
+
+};
+
+const showBigPicture = (url, likes, comments, description) => {
+  currentDisplayedComments = 0;
+  commentsList.innerHTML = '';
+  scrollBackground.classList.add('modal-open');
+
+  renderBigPicture(url, likes, comments, description);
+
+  document.addEventListener('keydown', onBigPuctureEscpaeKey);
+  bigPicture.classList.remove('hidden');
+};
+
+
+bigPictureCancel.addEventListener('click', (evt) => {
+  userBigPictureCloseElement(evt);
+
+});
+
+showMoreCommentsButton.addEventListener('click', showMoreComments);
+
+
+export {renderComment, renderComments, showBigPicture};
---
+++ 10/js/thumbnails.js
@@ -0,0 +1,25 @@
+import { showBigPicture } from './show-big-picture.js';
+
+const picturesContainer = document.querySelector('.pictures');
+const thumbnailTemplateContainer = document.querySelector('#picture').content.querySelector('.picture');
+
+
+const renderPhoto = ({url, likes, comments, description}) => {
+  const photoItem = thumbnailTemplateContainer.cloneNode(true); // клониурем
+  photoItem.querySelector('img').src = url;
+  photoItem.querySelector('.picture__likes').textContent = likes;
+  photoItem.querySelector('.picture__comments').textContent = comments.length;
+  photoItem.addEventListener('click', () => showBigPicture(url, likes, comments, description));
+  return photoItem;
+};
+
+const renderPhotos = (photos) => {
+  const similarPhotoFragment = document.createDocumentFragment(); // создаём фрагмент
+  photos.forEach((photo) => {
+    const onePhoto = renderPhoto(photo);
+    similarPhotoFragment.appendChild(onePhoto); // прикрепляем к фагменту
+  });
+  picturesContainer.appendChild(similarPhotoFragment); // приркепляем фото в контейнер
+};
+
+export {renderPhotos};
